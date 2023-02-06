f5e5cd71f5f340b87bf50a33ecf1263a04bdff2d
bongzniak <bongzniak@gmail.com>
2023-02-06 21:38:22 +0900

    feature: UIView template 추가

---
+++ reactorKit-filetemplates/Sources/template/View.xctemplate/Default/___FILEBASENAME___View.swift
@@ -0,0 +1,53 @@
+//___FILEHEADER___
+
+import UIKit
+
+import ReactorKit
+import RxCocoa
+import RxSwift
+import SnapKit
+import Then
+
+final class ___VARIABLE_productName___View: BaseView {
+    
+    // MARK: Constants
+    
+    private enum Metric {
+        static let height: CGFloat = .zero
+    }
+    
+    private enum Color {
+    }
+    
+    private enum Font {
+    }
+        
+    // MARK: Properties
+    
+    // MARK: UI Views
+    
+    // MARK: Initializing
+
+
+    // MARK: UI Setup
+    
+    override func setupProperty() {
+        super.setupProperty()
+    }
+    
+    override func setupDelegate() {
+        super.setupDelegate()
+    }
+    
+    override func setupHierarchy() {
+        super.setupHierarchy()
+    }
+    
+    override func setupLayout() {
+        super.setupLayout()
+    }
+
+    func setupBind() {
+
+    }
+}
